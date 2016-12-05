642aba67ec884a6ea996d3579b9ff3fdeff8ce07
mallikarjunveepuru <malliveepuru@gmail.com>
2016-12-05 16:37:24 +0530

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,3 @@ license          'All rights reserved'
 description      'Installs/Configures java_app'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends 'mysql', '~> 6.0'
